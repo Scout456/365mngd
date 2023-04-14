057a4e9b1fdfe3c257197b5fc0764518a76cd2d4
rob <brubachr@gmail.com>
2023-04-14 10:19:40 -0700

    added onload function

--- index.js
+++ index.js
@@ -49,6 +49,58 @@ const theOptions = {
 }); 
 
 
+/*Intersection Observer Function*/
+function myFunction(){
+const theOptions = { 
+
+  root:null, 
+
+  threshold:0.2, 
+
+  rootMargin:"0px 0px 0px 0px"
+
+
+   }; 
+
+  const observer = new IntersectionObserver(entries => { 
+
+  entries.forEach(entry => { 
+
+    if(entry.isIntersecting){ 
+
+      entry.target.classList.add('in-view'); 
+
+      observer.unobserve(entry.target); 
+
+    } 
+
+  }); 
+
+}, theOptions); 
+
+ window.addEventListener('DOMContentLoaded',(event) =>{ 
+
+ /* const sections = Array.from(document.getElementsByClassName('section'));
+   for (let section of sections) {
+     const children = section.getElementsByClassName('fade-delay');
+     for (let count = 0; count < children.length; count++) {
+        children[count].setAttribute('style', 'transition-delay: ' + count * 0.5 + 's');
+    }
+  observer.observe(section);
+}
+});*/
+
+  const sections = Array.from(document.getElementsByClassName('section')); 
+
+  for (let section of sections){ 
+
+    observer.observe(section); 
+
+  } 
+
+}); 
+}
+
 /*--------Submit form response on button----------------------------------------*/
 
 function get_add(){
