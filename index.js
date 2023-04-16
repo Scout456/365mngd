dcf438971eac58c42421df1137401bdaa9ad3db6
rob <brubachr@gmail.com>
2023-04-15 17:29:18 -0700

    changed rootMargin

--- index.js
+++ index.js
@@ -5,7 +5,7 @@
 
   threshold:0, 
 
-  rootMargin:"-50px 0px -50px 0px"
+  rootMargin:"-50px -50px -50px -50px"
 
 
    }; 
@@ -27,17 +27,6 @@
 }, theOptions); 
 
  window.addEventListener('DOMContentLoaded',(event) =>{ 
-
- /* const sections = Array.from(document.getElementsByClassName('section'));
-   for (let section of sections) {
-     const children = section.getElementsByClassName('fade-delay');
-     for (let count = 0; count < children.length; count++) {
-        children[count].setAttribute('style', 'transition-delay: ' + count * 0.5 + 's');
-    }
-  observer.observe(section);
-}
-});*/
-
 
   const sections = Array.from(document.getElementsByClassName('section')); 
 
