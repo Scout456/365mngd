40182b2e399bed9ae40cc4b0df1ae677b1c38562
rob <brubachr@gmail.com>
2023-05-21 20:46:59 -0700

    finished stepsFacts

--- index.js
+++ index.js
@@ -39,6 +39,39 @@
 }); 
 
 
+/*Background color change-------------------------------------------*/
+
+          /*  var body = document.getElementsByTagName('body')[0];
+            body.style.backgroundColor = 'whitesmoke';
+         
+       
+            
+
+          function pageScroll() { 
+            var scroll = window.pageYOffset;
+            if (scroll < 300) {
+                // blue
+                body.style.backgroundColor = '#2D68C4';
+            } else if (scroll >= 300 && scroll < 1200) {
+                // blue
+                body.style.backgroundColor = '#0070AC';
+            } else if (scroll >= 1200 && scroll < 2000) {
+                // light blue
+                body.style.backgroundColor = '#1ca9c9';
+            } else if (scroll >= 2000 && scroll < 3500) {
+                // Air Superiority Blue
+                body.style.backgroundColor = '#72A0C1';
+                
+            } else if (scroll >= 3500 && scroll < 5000) {
+                // magic blue
+                body.style.backgroundColor = '#0077c0';
+            } else {
+                // orange yellow
+                body.style.backgroundColor = '#C0821D';
+            }
+        }
+
+*/
 
 
 
